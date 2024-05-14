7a060bf28b9ad2afff3edc1ed9fd7f9ab08362dd
Maria Durrani <maria_durrani@hotmail.com>
2024-05-14 11:19:42 +0200

    Refactoring the media queries in the wrapper.vue

--- Portfolio/src/Wrapper.vue
+++ Portfolio/src/Wrapper.vue
@@ -13,22 +13,13 @@
     font-family: "Raleway";
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    margin: 2rem;
-}
 
-@media (min-width: 280px) and (max-width: 767px) {
-    .wrapper {
-        align-items: center;
-        margin: 5rem;
+    @media (min-width: 768px) and (max-width: 1899px) {
+        margin: 3rem;
+    }
+
+    @media (min-width: 1900px) {
+        margin: 3rem 5rem 0 5rem;
     }
 }
-
-@media (min-width: 1900px) {
-    .wrapper {
-        margin: 3rem 8rem 0 8rem;
-    }
-}
-
-@media (min-width: 768px) and (max-width: 1024px) {}
 </style>
