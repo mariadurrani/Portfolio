8d986aa0cba36bb3e516d4b47d39afbb2c1a0623
Maria Durrani <maria_durrani@hotmail.com>
2024-05-22 19:55:56 +0200

    Set up a text container

--- Portfolio/src/views/WorkView.vue
+++ Portfolio/src/views/WorkView.vue
@@ -1,7 +1,30 @@
+<script setup lang="ts">
+import Wrapper from '@/Wrapper.vue';
+</script>
+
 <template>
-    <div class="work">
-        <h1>This is a work page</h1>
-    </div>
+    <Wrapper>
+        <main class="work-page-wrapper">
+            <div class="work-page-text-container">
+                <p class="work-page-title">Work</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In adipisci, quia ipsa voluptatibus deleniti
+                    hic
+                    temporibus harum, repudiandae sint, provident error aut ullam nihil laboriosam suscipit odio
+                    excepturi
+                    ratione est.</p>
+            </div>
+        </main>
+    </Wrapper>
 </template>
 
-<style></style>
+<style>
+.work-page-text-container {
+    /* font-family: "TitilliumWeb"; */
+    margin: 7rem 2rem;
+}
+
+.work-title {
+    text-transform: uppercase;
+    font-weight: 600;
+}
+</style>
