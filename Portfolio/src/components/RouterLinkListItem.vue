a9d09c5229b3746cd1d38e7660026fb7381d2709
Maria Durrani <maria_durrani@hotmail.com>
2024-05-22 16:22:06 +0200

    Added a router link list item component and passed props dynamically

---
+++ Portfolio/src/components/RouterLinkListItem.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+interface Props {
+    to: string;
+    className: string;
+    text: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <li>
+        <RouterLink :to="props.to" :class="props.className">{{ props.text }}</RouterLink>
+    </li>
+</template>
+
+<style>
+</style>
