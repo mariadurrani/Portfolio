96c6168af00a65f43041b7011da2c398a0614e61
Maria Durrani <maria_durrani@hotmail.com>
2024-05-23 11:23:03 +0200

    Set up media queries to text container

--- Portfolio/src/views/WorkView.vue
+++ Portfolio/src/views/WorkView.vue
@@ -7,24 +7,12 @@ import Wrapper from '@/Wrapper.vue';
         <main class="work-page-wrapper">
             <div class="work-page-text-container">
                 <p class="work-page-title">Work</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In adipisci, quia ipsa voluptatibus deleniti
-                    hic
-                    temporibus harum, repudiandae sint, provident error aut ullam nihil laboriosam suscipit odio
-                    excepturi
-                    ratione est.</p>
+                <p class="work-page-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. In adipisci, quia
+                    ipsa voluptatibus deleniti hic temporibus harum, repudiandae sint, provident error aut ullam nihil
+                    laboriosam suscipit odio excepturi ratione est.</p>
             </div>
         </main>
     </Wrapper>
 </template>
 
-<style>
-.work-page-text-container {
-    /* font-family: "TitilliumWeb"; */
-    margin: 7rem 2rem;
-}
-
-.work-title {
-    text-transform: uppercase;
-    font-weight: 600;
-}
-</style>
+<style src="../assets/WorkView.scss"></style>
