54b05da13c4b710ce5745722bf49831fe61a5ff3
Maria Durrani <maria_durrani@hotmail.com>
2024-06-02 17:57:18 +0200

    Fixed some things

--- Portfolio/src/views/HomeView.vue
+++ Portfolio/src/views/HomeView.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
 import Wrapper from "@/Wrapper.vue";
 import { homeViewContent } from "./home-view-content";
-
-
+;
 </script>
 
 <template>
     <Wrapper>
         <header class="home-page__wrapper">
             <div class="home-page__introduction">
-                <p class="home-page__introduction__hi">Hi! I'm</p>
+                <p class="home-page__introduction__hi ">Hi! I'm</p>
                 <p class="home-page__introduction__name">{{ homeViewContent.firstName + " " + homeViewContent.lastName
                     }}</p>
             </div>
             <div class="home-page__career">
                 <p class="home-page__career__p">A</p>
-                <p class="home-page__career__title">Frontend developer</p>
+                <p class="home-page__career__title ">{{ homeViewContent.titleCareer }}</p>
             </div>
         </header>
     </Wrapper>
