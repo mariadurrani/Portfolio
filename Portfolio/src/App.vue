bc5c0db2f66896ef86ca7d3a8edcf6609f28ddeb
Maria Durrani <maria_durrani@hotmail.com>
2024-06-02 18:18:24 +0200

    Nested HomeView

--- Portfolio/src/App.vue
+++ Portfolio/src/App.vue
@@ -14,8 +14,8 @@ const isMenuShown = ref(false)
     <Wrapper>
         <header>
             <div class="router-wrapper">
-                <button @click="isMenuShown = !isMenuShown" class="menu-button-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="menu-button">
+                <button @click="isMenuShown = !isMenuShown" class="menu-button__container">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="menu-button__container__item">
                         <path v-if="!isMenuShown"
                             d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
                         <path v-else="isMenuShown" fill="#ffffff"
@@ -29,13 +29,8 @@ const isMenuShown = ref(false)
             </div>
         </header>
 
-        <nav v-if="isMenuShown" class="opened-menu">
+        <nav v-if="isMenuShown" class="menu__mobile__opened">
             <RouterLinkMenu />
-            <!-- Remove this below in another branch (not needed) and position the menu links in the middle -->
-            <div class="menu-fullname">
-                <h1 class="menu-firstName">{{ homeViewContent.firstName }}</h1>
-                <h1 class="menu-lastName">{{ homeViewContent.lastName }}</h1>
-            </div>
         </nav>
         <RouterView />
     </Wrapper>
