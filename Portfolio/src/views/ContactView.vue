873d160850a942570094363ae0f94b6fda1f42e7
Maria Durrani <maria_durrani@hotmail.com>
2024-06-18 15:54:04 +0200

    made a reactive form

--- Portfolio/src/views/ContactView.vue
+++ Portfolio/src/views/ContactView.vue
@@ -1,6 +1,68 @@
 <template>
-    <div class="contact">
-        <h1>This is a contact page</h1>
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" id="name" v-model="form.name" />
+                <span v-if="errors.name">{{ errors.name }}</span>
+            </div>
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" id="email" v-model="form.email" />
+                <span v-if="errors.email">{{ errors.email }}</span>
+            </div>
+            <div>
+                <label for="message">Message:</label>
+                <textarea id="message" v-model="form.message"></textarea>
+                <span v-if="errors.message">{{ errors.message }}</span>
+
+            </div>
+            <button type="submit">Submit</button>
+        </form>
     </div>
 </template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+    setup() {
+        const form = reactive({
+            name: '',
+            email: '',
+            message: ''
+        });
+
+        const errors = reactive({
+            name: '',
+            email: '',
+            message: ''
+        });
+
+        const validateForm = () => {
+            errors.name = form.name ? '' : 'Name is required';
+            errors.email = form.email ? '' : 'Email is required';
+            errors.message = form.message ? '' : 'Message is required';
+
+            return !errors.name && !errors.email && !errors.message;
+        }
+
+        const handleSubmit = () => {
+            if (validateForm()) {
+                console.log('Form submitted:', form);
+            } else {
+                console.log("Some fields are not filled out correctly")
+
+            }
+        };
+
+        return {
+            form,
+            errors,
+            handleSubmit
+        };
+    }
+};
+</script>
+
 <style></style>
