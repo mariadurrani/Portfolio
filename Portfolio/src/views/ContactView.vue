2414afe9f9e1998874f5080365cc3f4406b3d3d1
Maria Durrani <maria_durrani@hotmail.com>
2024-06-20 15:12:31 +0200

    Finished styling contact page

--- Portfolio/src/views/ContactView.vue
+++ Portfolio/src/views/ContactView.vue
@@ -1,15 +1,26 @@
 <template>
     <div class="contact-page__wrapper">
-        <div class="contact-page__text">
-            <p class="contact-page__text__contact-p">Contact</p>
-            <hr class="contact-page__text__hr">
-            <div class="contact-page__email-container">
-                <p>E-MAIL</p>
-                <a href="mailto:maria_durrani@hotmail.com">maria_durrani@hotmail.com</a>
+        <div class="contact-page__content-area">
+            <p class="contact-page__headline">Contact</p>
+            <hr class="contact-page__seperator">
+            <div class="contact-page__options">
+                <div class="contact-page__email">
+                    <p class="contact-page__email__text">E-mail</p>
+                    <a href="mailto:maria_durrani@hotmail.com"
+                        class="contact-page__email__link">maria_durrani@hotmail.com</a>
+                </div>
+                <div class="contact-page__social-media">
+                    <p class="contact-page__social-media__text">Social Media</p>
+                    <ul class="contact-page__social-media__links">
+                        <li><a href="https://www.linkedin.com/in/maria-durrani00/"
+                                class="contact-page__linkedin">LinkedIn</a></li>
+                        <li><a href="https://github.com/mariadurrani" class="contact-page__github">GitHub</a></li>
+                    </ul>
+                </div>
             </div>
-
         </div>
     </div>
+
     <!-- <div>
         <form @submit.prevent="handleSubmit">
             <div>
