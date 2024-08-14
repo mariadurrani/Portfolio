53e68f5cc6d04a2e3b4ab6287dc3ab061168123b
Maria Durrani <maria_durrani@hotmail.com>
2024-08-14 19:11:10 +0200

    Done with standard css

--- Portfolio/src/views/AboutView.vue
+++ Portfolio/src/views/AboutView.vue
@@ -1,9 +1,26 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
+    <div class="about-page__wrapper">
+        <div class="about-page__content">
+            <div class="about-page__img__wrapper ">
+                <img src="../assets/images/me.png" class="about-page__content__img">
+            </div>
+            <div class="about-page__content__text">
+                <div class="about-page__content__text__heading">
+                    <p class="about-page__content__text__heading__one">About</p>
+                    <p class="about-page__content__text__heading__two">Maria <b>Durrani</b></p>
+
+                </div>
+                <p class="about-page__content__text__paragraph">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, quia nam eaque deserunt quam
+                    libero, ex quo dolorum aperiam sequi fuga laborum, repellat ipsam iusto praesentium atque id
+                    asperiores perferendis.
+                </p>
+            </div>
+        </div>
     </div>
 </template>
-<style></style>
+
+<style src="../assets/AboutView.scss"></style>
 
 <script>
 </script>
